658da0968407ef4573dc84c34ce8880b0ffd8b87
DejanNikolic003 <knolekjhnny@gmail.com>
2025-08-04 11:54:35 +0200

    Added loginUser api call.

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -1,7 +1,14 @@
 import { fetchClient } from "./fetchClient";
 
-export const createUser = async (data) => {
+export const registerUser = async (data) => {
   return await fetchClient("/register", {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+};
+
+export const loginUser = async (data) => {
+  return await fetchClient("/login", {
     method: "POST",
     body: JSON.stringify(data),
   });
