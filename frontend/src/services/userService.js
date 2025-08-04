dcc3219dedb6a85247a75dd7a8b10be607cea49a
DejanNikolic003 <knolekjhnny@gmail.com>
2025-08-04 11:54:46 +0200

    Added user logging to userService.

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,9 +1,20 @@
-import { createUser } from "../api/userApi";
+import { registerUser, loginUser } from "../api/userApi";
 
-export const registerUser = async (formData) => {
+export const register = async (formData) => {
   // eslint-disable-next-line no-useless-catch
   try {
-    const user = await createUser(formData);
+    const user = await registerUser(formData);
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const login = async (formData) => {
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const user = await loginUser(formData);
 
     return user;
   } catch (error) {
