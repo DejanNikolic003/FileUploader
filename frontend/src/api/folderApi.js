ee14630fc36f139273487c1266a7f7eb36e03292
DejanNikolic003 <knolekjhnny@gmail.com>
2025-08-04 14:04:48 +0200

    created API calls for CRUD

---
+++ frontend/src/api/folderApi.js
@@ -0,0 +1,53 @@
+import { fetchClient } from "./fetchClient";
+
+export const getFolders = async (token) => {
+  if (!token) return;
+
+  return await fetchClient("/folders/", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const createFolder = async (token, data) => {
+  if (!token) return;
+
+  return await fetchClient("/folders/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      folderName: data,
+    }),
+  });
+};
+
+export const deleteFolderById = async (token, id) => {
+  if (!token) return;
+
+  return await fetchClient(`/folders/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const editFolderById = async (token, id, data) => {
+  if (!token) return;
+
+  return await fetchClient(`/folders/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      folderName: data,
+    }),
+  });
+};
