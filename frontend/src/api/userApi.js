f920f93b6692012a6c92771a91229b46516287af
DejanNikolic003 <knolekjhnny@gmail.com>
2025-08-04 14:05:00 +0200

    Changed function names, removed user suffix.

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -1,13 +1,13 @@
 import { fetchClient } from "./fetchClient";
 
-export const registerUser = async (data) => {
+export const register = async (data) => {
   return await fetchClient("/register", {
     method: "POST",
     body: JSON.stringify(data),
   });
 };
 
-export const loginUser = async (data) => {
+export const login = async (data) => {
   return await fetchClient("/login", {
     method: "POST",
     body: JSON.stringify(data),
