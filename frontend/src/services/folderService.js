192ba7aef4e9347b64ae82e55a4dd7dc4db162a8
DejanNikolic003 <knolekjhnny@gmail.com>
2025-08-06 17:48:30 +0200

    Added getFolderById fn.

--- frontend/src/services/folderService.js
+++ frontend/src/services/folderService.js
@@ -1,9 +1,30 @@
 import * as folderAPI from "../api/folderApi";
 
+//   const response = await fetch(`${API_URL}/folders/${id}`, {
+//       headers: {
+//         Authorization: `Bearer ${token}`,
+//       },
+//     });
+
+//     const result = await response.json();
+
+//     if (!response.ok) {
+//       navigate("/folders");
+//       return;
+//     }
+
+//     setFolder(result.folder);
+
 export const getFolders = async (token) => {
   const { folders } = await folderAPI.getFolders(token);
 
   return folders;
+};
+
+export const getFolderById = async (token, id) => {
+  const result = await folderAPI.getFolderById(token, id);
+
+  return result;
 };
 
 export const createFolder = async (token, data) => {
