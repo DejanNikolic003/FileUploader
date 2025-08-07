1190c312d1e49bc7fb967fc52447840833eca3eb
DejanNikolic003 <knolekjhnny@gmail.com>
2025-08-07 14:30:45 +0200

    logout , verifytoken added

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -4,6 +4,7 @@ export const register = async (data) => {
   return await fetchClient("/register", {
     method: "POST",
     body: JSON.stringify(data),
+    credentials: "include",
   });
 };
 
@@ -11,5 +12,20 @@ export const login = async (data) => {
   return await fetchClient("/login", {
     method: "POST",
     body: JSON.stringify(data),
+    credentials: "include",
+  });
+};
+
+export const logout = async () => {
+  return await fetchClient("/logout", {
+    method: "POST",
+    credentials: "include",
+  });
+};
+
+export const verifyToken = async () => {
+  return await fetchClient(`/token`, {
+    method: "POST",
+    credentials: "include",
   });
 };
