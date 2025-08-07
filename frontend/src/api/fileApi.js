166b8854481df1eeed2d52c70104d14479deccea
DejanNikolic003 <knolekjhnny@gmail.com>
2025-08-07 12:50:57 +0200

    Added file download api call.

---
+++ frontend/src/api/fileApi.js
@@ -0,0 +1,36 @@
+import { fetchClient } from "./fetchClient";
+
+export const getFiles = async (token, id) => {
+  return await fetchClient(`/files/folders/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const uploadFile = async (token, data) => {
+  return await fetchClient(
+    `/files/upload`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(data),
+    },
+    null,
+  );
+};
+
+export const downloadFile = async (token, id) => {
+  return await fetchClient(
+    `/files/${id}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+    null,
+    "blob",
+  );
+};
---
+++ frontend/src/services/fileService.js
@@ -0,0 +1,21 @@
+import * as fileApi from "../api/fileApi";
+
+export const getFiles = async (token, id) => {
+  const { files } = await fileApi.getFiles(token, id);
+
+  return files;
+};
+
+export const uploadFile = async (token, data) => {
+  const result = await fileApi.uploadFile(token, data);
+
+  // console.log(result);
+
+  return result;
+};
+
+export const downloadFile = async (token, id) => {
+  const result = await fileApi.downloadFile(token, id);
+
+  return result;
+};
